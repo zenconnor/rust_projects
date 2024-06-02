ea04f636e89d1aeada27691493c6d9b6b1c8cb6d
Connor Elkin <celkin3@gatech.edu>
2024-06-02 17:23:40 -0500

    practicing ownership

---
+++ ownership/target/debug/deps/ownership-0f2d8d34190ade33.d
@@ -0,0 +1,5 @@
+/Users/connorelkin/dev/rust/rust_projects/ownership/target/debug/deps/ownership-0f2d8d34190ade33: src/main.rs
+
+/Users/connorelkin/dev/rust/rust_projects/ownership/target/debug/deps/ownership-0f2d8d34190ade33.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/connorelkin/dev/rust/rust_projects/ownership/target/debug/ownership: /Users/connorelkin/dev/rust/rust_projects/ownership/src/main.rs
